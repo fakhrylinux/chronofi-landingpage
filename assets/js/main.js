c00a16bb00d89f8502846a7db9a0b7da370deb10
fakhry <fakhry.linux@gmail.com>
2022-04-04 13:40:58 +0900

    remove overflow-x: hidden because this disable scroll transparent

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,24 @@
-// animate lazy loading image
-gsap.defaults({ ease: 'power1' })
-gsap.set('.lazy', { y: 50, opacity: 0 })
+// window.addEventListener('DOMContentLoaded', event => {
+//
+// // Activate Bootstrap scrollspy on the main nav element
+//   const mainNav = document.body.querySelector('#navbar')
+//   if (mainNav) {
+//     new bootstrap.ScrollSpy(document.body, {
+//       target: '#navbar',
+//       offset: 74,
+//     })
+//   }
 
-ScrollTrigger.batch('.lazy', {
-  onEnter: batch => gsap.to(batch,
-    { opacity: 1, y: 0, stagger: 0.5 }),
-  // onLeaveBack: batch => gsap.to(batch,
-  //   { opacity: 0, y:100, stagger: 0.5 }),
-})
+
+// animate lazy loading image
+  gsap.defaults({ ease: 'power1' })
+  gsap.set('.lazy', { y: 50, opacity: 0 })
+
+  ScrollTrigger.batch('.lazy', {
+    onEnter: batch => gsap.to(batch,
+      { opacity: 1, y: 0, stagger: 0.5 }),
+    // onLeaveBack: batch => gsap.to(batch,
+    //   { opacity: 0, y:100, stagger: 0.5 }),
+  })
+
+// })
